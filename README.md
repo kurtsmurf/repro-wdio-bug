13c97f9d7575633b56325725b09f66c6192676dd
Chris Smith <csmith@legnasoftware.com>
2021-02-26 12:13:00 -0500

    Add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# repro-wdio-bug
+
+Minimal working example of a bug I'm struggling with 😢 when writing tests with [webdriverio](webdriver.io).
+
+To reproduce, run `npm run serve` to serve the website under test, then run `npm test`.
+
+See [test source](./test/specs/nested-table-test.js) for bug details.
--- test/specs/nested-table-test.js
+++ test/specs/nested-table-test.js
@@ -2,10 +2,13 @@ describe('bug', () => {
     it('selects theader row instead of tbody row', () => {
         browser.url(`http://localhost:8081`);
 
-        // I expect this combination of selectors to return the 
-        // the first body row of the inner table.
-        // Instead, it returns the first head row of the inner
-        // table.
+        /* 
+        I expect this combination of selectors to return the 
+        the first row of the inner table <tbody>.
+
+        Instead, it returns the first row of the inner
+        table <thead>.
+        */
         const innerTablerow = $('#inner-table').$('tbody tr')
         expect(innerTablerow.getText())
             .toEqual('inner-table-first-row-cell')
